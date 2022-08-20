fe8bf6aea351ff642340d23a127667c2b2b399b8
Samwel Waruhiu <samweliwaruhiu@gmail.com>
2022-08-20 21:20:16 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+
+
+function superbowlWin(record){
+  const findItems = record.find(rray => rray.result === "W");
+  if (findItems){
+    return findItems.year;
+  }
+   }
+
